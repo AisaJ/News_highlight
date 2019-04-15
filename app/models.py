ca226d931d1d27fd66ba714040939a48f91fc4d3
Aisa <aisajemila@gmail.com>
2019-04-15 13:39:16 +0300

    Added function to request for the articles page details.

--- app/models.py
+++ app/models.py
@@ -4,9 +4,23 @@ class News:
   '''
   def __init__(self,id,name,description,url,category,language):
     
-    self.author = id
+    self.id = id
     self.name = name
     self.description = description
     self.url = url
     self.category = category
     self.language = language
+
+
+class Articles:
+  '''
+  Articles class for defining Articles objects
+  '''
+  def __init__(self,id, author,title,description,image,date,content):
+    self.id = id
+    self.author = author
+    self.title = title
+    self.description = description
+    self.image = image
+    self.date = date
+    self.content = content
--- app/requests.py
+++ app/requests.py
@@ -7,11 +7,14 @@ apiKey = None
 #Getting the news base url
 base_url = None
 
+#Getting sources base url
+src_base_url = None
+
 def configure_request(app):
-  global apiKey,base_url
+  global apiKey,base_url, src_base_url
   apiKey = app.config['NEWS_API_KEY']
   base_url = app.config['NEWS_API_BASE_URL']
-
+  src_base_url = app.config['SOURCE_API_BASE_URL']
 
 def get_news():
   '''
@@ -52,21 +55,34 @@ def process_sources(news_list):
   return news_sources
 
 def get_source(id):
-  get_source_details_url = base_url.format(id,apiKey)
+  get_source_details_url = src_base_url.format(id,apiKey)
 
   with urllib.request.urlopen(get_source_details_url) as url:
     source_details_data = url.open()
     source_details_response = json.loads(source_details_data)
 
-    news_object = None
-    if source_details_response:
-      id = source_details_response.get('id')
-      name = source_details_response.get('name')
-      description = source_details_response.get('description')
-      url = source_details_response.get('url')
-      category = source_details_response.get('categry')
-      language = source_details_response.get('language')
+    articles_object = None
+    if articles_results['articles']:
+      articles_results = process_articles(articles_results['articles'])
 
-      news_object = Sources(id,name,description,url,category,language)
+  return articles_object
 
-  return news_object
+def process_articles(sources_list):
+  '''
+  Function that processes the articles to objects
+  '''
+  articles_object = []
+  for article_item in sources_list:
+    id = article_item.get('id')
+    author = article_item.get('author')
+    title = article_item.get('title')
+    description = article_item.get('description')
+    image = article_item.get('urlToImage')
+    date = article_item.get('date')
+    content = article_item.get('content')
+
+    if image:
+      articles_results = Articles (id,author,title,description,image,date, content)
+
+
+
